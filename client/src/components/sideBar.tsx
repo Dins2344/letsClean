38d482b86f559d08727fd2bc9c9c5325faf8cc58
Dinson Davis <117897698+Dins2344@users.noreply.github.com>
2023-11-29 12:27:03 +0530

    package adding v2

--- client/src/components/sideBar.tsx
+++ client/src/components/sideBar.tsx
@@ -1,24 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Package } from "../types";
 
 interface Props {
   packages: Package[];
 }
 
+interface orderData{
+  packages: { name: string, price: string }[],
+  totalAmount:number
+}
+
 const SideBar: React.FC<Props> = ({ packages }) => {
+  const [orderTotal,setOrderTotal] = useState(0)
   useEffect(() => {
-    console.log(packages);
-  }, [packages]);
+    calculateTotal()
+  }, [packages])
+  
+  const calculateTotal = () => {
+    let total = 0
+    packages.forEach((item) => {
+      if (item.price.length) {
+        total = parseInt(total.toString()) + parseInt(item.price)
+      }
+    })
+    setOrderTotal(total)
+  }
+
+  const handleCheckout = () => {
+    const data:orderData = { packages: [], totalAmount: orderTotal }
+    packages.forEach((item) => {
+      if (item.name.length) {
+        data.packages.push(item)
+      }
+    })
+
+    console.log(data)
+  }
+
   return (
     <>
-      <div className="w-full flex flex-col">
-        {packages.map((item,i) => {
+      <div className="w-full flex flex-col px-5">
+        {packages.map((item, i) => {
           return (
-            
-              <h3 key={i}>{item.name}</h3>
-            
+            <div key={i} className="flex justify-between">
+              <h3>{item.name}</h3>
+              <h4>{item.price}</h4>
+            </div>
           );
         })}
+        {orderTotal >= 0 && <h3>total amount: {orderTotal}</h3>}
+        
+        <button onClick={handleCheckout}>checkout</button>
       </div>
     </>
   );
